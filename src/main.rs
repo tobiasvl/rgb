9b93b7de61600517d7d8fdba95b4daf139566556
Tobias V. Langhoff <tobias@langhoff.no>
2024-01-10 23:36:16 +0100

    Bump dependency (clap)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-extern crate clap;
-use clap::{App, Arg};
+use clap::Parser;
+use std::path::PathBuf;
 
 mod bus;
 mod cpu;
@@ -9,41 +9,31 @@ use bus::{Bus, MBCKind, Mbc};
 use cpu::{Cpu, Flags, RegisterPair, Registers};
 use ppu::Ppu;
 
-fn main() {
-    let matches = App::new("RGB")
-        .version("1.0")
-        .author("Tobias V. Langhoff <tobias@langhoff.no>")
-        .about("Game Boy emulator")
-        .arg(
-            Arg::with_name("ROM")
-                .help("ROM file")
-                .required(true) // for the time being
-                .index(1),
-        )
-        .arg(
-            Arg::with_name("bootrom")
-                .short("b")
-                .long("bootrom")
-                .help("Boot ROM file")
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("debug")
-                .short("d")
-                .long("debug")
-                .takes_value(false)
-                .help("Debug log"),
-        )
-        .get_matches();
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Game Boy ROM file
+    #[arg(index = 1, value_name = "ROM")]
+    rom: PathBuf,
 
-    let debug = matches.is_present("debug"); // = args.len() > 1 && args[1] == "debug";
+    /// Game Boy Boot ROM file
+    #[arg(short, long, value_name = "FILE")]
+    bootrom: Option<PathBuf>,
+
+    /// Log debugging information to stdout
+    #[arg(short, long)]
+    debug: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
 
     let mut cpu = Cpu {
         bus: Bus {
             bootrom_enabled: false,
             cartridge: Mbc {
                 kind: MBCKind::NoMBC,
-                rom: [0; 32768],
+                rom: [0; 0x8000],
                 ram: [0; 0x2000],
                 ram_enabled: false,
                 active_bank: 0,
@@ -81,7 +71,7 @@ fn main() {
         ime: false,
     };
 
-    if !match matches.value_of("bootrom") {
+    if !match cli.bootrom {
         Some(bootrom_file) => match std::fs::read(bootrom_file) {
             Ok(bootrom) => {
                 cpu.bus.bootrom[0..=0xFF].clone_from_slice(&bootrom[..]);
@@ -110,7 +100,7 @@ fn main() {
         cpu.registers.sp = 0xFFFE;
     };
 
-    let rom = std::fs::read(matches.value_of("ROM").unwrap()).expect("Couldn't load ROM");
+    let rom = std::fs::read(cli.rom).expect("Unable to open ROM");
 
     cpu.bus.cartridge.rom[..].clone_from_slice(&rom[..]);
 
@@ -118,7 +108,7 @@ fn main() {
 
     loop {
         // gucci:
-        if debug {
+        if cli.debug {
             println!("A:{:02X} F:{:02X} B:{:02X} C:{:02X} D:{:02X} E:{:02X} H:{:02X} L:{:02X} SP:{:04X} PC:{:04X} PCMEM:{:02X},{:02X},{:02X},{:02X}",
                 cpu.registers.a,
                 cpu.get_register_pair(&RegisterPair::AF) & 0xFF,
